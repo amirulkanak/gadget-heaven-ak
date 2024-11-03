6309f5263129c72e50448ca17da16d9e4aee451f
amirulkanak <coder.kanak@gmail.com>
2024-11-04 01:49:53 +0600

    Hero Banner Section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,17 @@
+import HeroBG from '../assets/images/hero-banner.png';
+
 const Header = () => {
-  return <header>Header Section</header>;
+  return (
+    <header className="max-width-wrapper">
+      <div className="p-6 max-w-[69.375rem] w-full max-h-[38.1875rem] border-[3px] border-solid border-rose-600 rounded-[2rem]">
+        <img
+          className="h-full w-full object-cover rounded-[2rem]"
+          src={HeroBG}
+          alt="VR"
+        />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
